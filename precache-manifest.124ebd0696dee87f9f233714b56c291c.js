d781b7f2b3ee70069d4d5d8e08c4e0edb3dcf29a
Charley Oliveira <charleycesar@gmail.com>
2020-11-08 19:22:11 -0300

    Updates

---
+++ precache-manifest.124ebd0696dee87f9f233714b56c291c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a38fb5010a8baa3a4e046cebde70960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069b8f36e5bd3030556d",
+    "url": "/static/css/main.484f9125.chunk.css"
+  },
+  {
+    "revision": "9621603187a36bbc7e94",
+    "url": "/static/js/2.1fae1dbe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1fae1dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069b8f36e5bd3030556d",
+    "url": "/static/js/main.425bad21.chunk.js"
+  },
+  {
+    "revision": "9c8c20a41cf2959ce919",
+    "url": "/static/js/runtime-main.ce80774d.js"
+  },
+  {
+    "revision": "6cd1bea940312ae02d7fa2e3c8aabe43",
+    "url": "/static/media/avatar.6cd1bea9.png"
+  },
+  {
+    "revision": "e654666b9a795b655fd19dbdb3f30c21",
+    "url": "/static/media/avatar2.e654666b.png"
+  }
+]);
